072a5ca83420612f2c7bb0addcc9dced97b2e869
alikaya-pcci <ali.kaya@pccinnovation.org>
2022-05-07 23:32:26 +0000

    REFACTOR Data Validation process
    Why: The program was built on the claim view in the first stage. However
    the pharmacy view will be introduced; thus, the code was refactored
    in the way that it will effectively accomodate both views
    
    How: A new class ViewDataValidation is introduced. This class is the
    parent class for both claim and pharmacy view data validation classes.
    Also, each process to validate data is constructed as a separate class
    to ease the modificaion process, if new ideas are introduced for each
    process.
    
    Tags:

---
+++ asthma/claim_data_validation_cls.py
@@ -0,0 +1,114 @@
+import re
+import warnings
+from pandas.testing import assert_index_equal
+
+
+class DiagnosisCodeValidation:
+
+    def __init__(self):
+        self.code_icd_columns = None
+        self._validated = False
+
+    @staticmethod
+    def _filter_code_and_icd_columns(df, group, icd=False):
+        if icd:
+            r = r'(?!.*admit)(?!.*desc)(?=.*icd)(?=.*{})'.format(group)
+        else:
+            r = r'(?!.*admit)(?!.*desc)(?!.*icd)(?=.*{})'.format(group)
+        pattern = re.compile(r)
+        return [col for col in df.columns if pattern.match(col)]
+
+    def _select_code_and_icd_columns(self, df):
+        d_cols = {}
+        for group in ['header_diagnosis', 'line_diagnosis', 'procedure']:
+            code_cols = self._filter_code_and_icd_columns(df, group)
+            icd_cols = self._filter_code_and_icd_columns(df, group, icd=True)
+            d_cols[group] = {'code': code_cols, 'icd': icd_cols}
+        self.code_icd_columns = d_cols
+        return d_cols
+
+    def _validate_diagnosis_and_procedure_code_columns(self, df):
+        d_cols = self._select_code_and_icd_columns(df)
+        for key, value in d_cols.items():
+            code_cols, icd_cols = value['code'], value['icd']
+            for code, icd in zip(code_cols, icd_cols):
+                code_null_idx = (df[code].loc[lambda x: x.str.strip() == '']
+                                 .index)
+                icd_null_idx = df[icd].loc[lambda x: x.isnull()].index
+
+                try:
+                    assert_index_equal(code_null_idx, icd_null_idx)
+                except AssertionError:
+                    if not set(icd_null_idx).issubset(set(code_null_idx)):
+                        msg = f'{code} needs manual missing values check!'
+                        raise ValueError(msg)
+
+    def get_code_icd_columns(self):
+        if self._validated is True:
+            return self._select_code_and_icd_columns()
+        else:
+            warnings.warn("Data haven't been validated. Use validate() method")
+            return None
+
+    def validate(self, df):
+        print('   Validating Diagnosis and Procedure Codes ...', end=' ')
+        if self._validated is False:
+            self._validate_diagnosis_and_procedure_code_columns(df)
+            self._validated = True
+            print('Done!')
+
+
+class ValidateRevenueCodes:
+
+    @staticmethod
+    def _validate_revenue_codes(revenue_codes):
+        assert revenue_codes.isnull().sum() < revenue_codes.shape[0]
+        assert revenue_codes.max() < 10_000
+        assert revenue_codes.min() > 99
+
+    def validate(self, revenue_codes):
+        print('   Validating Revenue Codes ...', end=' ')
+        self._validate_revenue_codes(revenue_codes)
+        print('Done!')
+
+
+class ValidatePlaceOfServiceCodes:
+
+    @staticmethod
+    def _validate_place_of_service_codes(place_of_service_codes):
+        arr = place_of_service_codes.str.strip().copy()
+        arr = arr.str.replace('Not Applicable', '00')
+        arr = arr.astype(int)
+        assert arr.loc[lambda x: x == 0].shape[0] < arr.shape[0]
+        assert arr.max() < 100
+        assert arr.min() >= 0
+
+    def validate(self, place_of_service_codes):
+        print('   Validating Place of Service Codes ...', end=' ')
+        self._validate_place_of_service_codes(place_of_service_codes)
+        print('Done!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- asthma/data_validation.py
+++ asthma/data_validation.py
@@ -1,12 +1,10 @@
 import os
-import re
-import warnings
 import pandas as pd
-from pandas.testing import assert_index_equal
 from asthma.validate_schema import ValidateSchema
+from asthma.claim_data_validation_cls import *
 
 
-class ClaimViewDataValidation:
+class ViewDataValidation:
 
     def __init__(self, filepath):
         if os.path.exists(filepath):
@@ -15,75 +13,7 @@ class ClaimViewDataValidation:
             raise FileNotFoundError('No such file in the given path.')
 
         self._df = None
-        self._validated = None
-
-    class DiagnosisCodeValidation:
-        def __init__(self, outer_class):
-            self._df = outer_class._df
-
-        def _filter_code_and_icd_columns(self, group, icd=False):
-            if icd:
-                r = r'(?!.*admit)(?!.*desc)(?=.*icd)(?=.*{})'.format(group)
-            else:
-                r = r'(?!.*admit)(?!.*desc)(?!.*icd)(?=.*{})'.format(group)
-            pattern = re.compile(r)
-            return [col for col in self._df.columns if pattern.match(col)]
-
-        def _select_code_and_icd_columns(self):
-            d_cols = {}
-            for group in ['header_diagnosis', 'line_diagnosis', 'procedure']:
-                code_cols = self._filter_code_and_icd_columns(group)
-                icd_cols = self._filter_code_and_icd_columns(group, icd=True)
-                d_cols[group] = {'code': code_cols, 'icd': icd_cols}
-            return d_cols
-
-        def _validate_diagnosis_and_procedure_code_columns(self):
-            print('   Validating Diagnosis and Procedure codes ...')
-            d_cols = self._select_code_and_icd_columns()
-            for key, value in d_cols.items():
-                code_cols, icd_cols = value['code'], value['icd']
-                for code, icd in zip(code_cols, icd_cols):
-                    code_null_idx = (self._df[code]
-                                     .loc[lambda x: x.str.strip() == '']
-                                     .index)
-                    icd_null_idx = (self._df[icd]
-                                    .loc[lambda x: x.isnull()]
-                                    .index)
-
-                    try:
-                        assert_index_equal(code_null_idx, icd_null_idx)
-                    except AssertionError:
-                        if not set(icd_null_idx).issubset(set(code_null_idx)):
-                            msg = f'{code} needs manual missing values check!'
-                            raise ValueError(msg)
-
-        def get_code_icd_columns(self):
-            return self._select_code_and_icd_columns()
-
-        def validate(self):
-            self._validate_diagnosis_and_procedure_code_columns()
-
-    class ValidateColumns:
-        def __init__(self, outer_class):
-            self._df = outer_class._df
-
-        def _validate_revenue_codes(self):
-            print('   Validating Revenue codes ...')
-            assert self._df.revenue_code.isnull().sum() < self._df.shape[0]
-            assert self._df.revenue_code.max() < 10_000
-            assert self._df.revenue_code.min() > 99
-
-        def _validate_place_of_service_codes(self):
-            print('   Validating Place of Service codes ...')
-            arr = self._df.place_of_service.str.strip().copy()
-            arr = arr.str.replace('Not Applicable', '00')
-            arr = arr.astype(int)
-            assert arr.max() < 100
-            assert arr.min() >= 0
-
-        def validate(self):
-            self._validate_revenue_codes()
-            self._validate_place_of_service_codes()
+        self._column_names = None
 
     def _validate_schema(self):
         if self._df is None:
@@ -95,29 +25,43 @@ class ClaimViewDataValidation:
         self._validate_schema()
         print('Reading data from the path ...')
         self._df = pd.read_parquet(self._filepath)
+        self._column_names = self._df.columns
+        self._df.columns = self._process_column_names()
 
     def _process_column_names(self):
         return [c.lower().strip().replace(' ', '_') for c in self._df.columns]
+
+    def get_raw_data(self):
+        if self._df is None:
+            self._read_data_from_filepath()
+        return self._df.rename(
+            columns={old: new
+                     for old, new in zip(self._df.columns, self._column_names)})
+
+
+class ClaimViewDataValidation(ViewDataValidation):
+
+    def __init__(self, filepath):
+        super().__init__(filepath)
+        self._validated = False
 
     def validate(self):
         if self._df is None:
             self._read_data_from_filepath()
 
-        self._df.columns = self._process_column_names()
-        ClaimViewDataValidation.DiagnosisCodeValidation(self).validate()
-        ClaimViewDataValidation.ValidateColumns(self).validate()
+        DiagnosisCodeValidation().validate(self._df)
+        ValidateRevenueCodes().validate(self._df.revenue_code)
+        ValidatePlaceOfServiceCodes().validate(self._df.place_of_service)
         self._validated = True
         print('Validation process completed!', end='\n\n')
 
-    def get_raw_data(self):
-        if self._df is None:
-            self._read_data_from_filepath()
-            return self._df
-        elif self._validated is True:
-            msg = 'Raw data no longer available. Use get_validated_data()'
-            warnings.warn(msg)
-            return None
-
     def get_validated_data(self):
-        if not self._validated: self.validate()
+        if not self._validated:
+            self.validate()
         return self._df
+
+
+class PharmacyViewDataValidation(ViewDataValidation):
+
+    def __init__(self, filepath):
+        super().__init__(filepath)
